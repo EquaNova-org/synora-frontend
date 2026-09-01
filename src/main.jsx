56363e38217eedbd5047feb24f43bfda0ba279f2
EquaNova-org <maxmudov.ozodbek77@gmail.com>
2026-09-01 17:04:03 +0500

    Integrate ClerkProvider for user authentication
    Added ClerkProvider for authentication and moved publishable key declaration.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { ClerkProvider } from "@clerk/clerk-react";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+const PUBLISHABLE_KEY = "pk_test_bG92aW5nLWJlYXItNDY2MC5jbGVyay5hY2NvdW50cy5kZXYk"; // move to .env before production
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <App />
+    </ClerkProvider>
+  </StrictMode>
+);
