8541fc21b81dd041a524345a198d6e2004d2c9ac
EquaNova-org <maxmudov.ozodbek77@gmail.com>
2026-09-01 17:07:57 +0500

    Refactor PUBLISHABLE_KEY to use env variable
    Updated PUBLISHABLE_KEY to use environment variable.

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { ClerkProvider } from "@clerk/clerk-react";
 import App from "./App.jsx";
 import "./index.css";
 
-const PUBLISHABLE_KEY = "pk_test_bG92aW5nLWJlYXItNDY2MC5jbGVyay5hY2NvdW50cy5kZXYk"; // move to .env before production
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
